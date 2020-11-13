98b2ed2f8a94515688971e7f6acb00ebb71468ca
cuitao <cuitao@aixuexi.com>
2020-11-13 16:01:43 +0800

    init repository

---
+++ Week1/src/main/java/com/tiantianbaobao/algorithm/Bootstrap.java
@@ -0,0 +1,13 @@
+package com.tiantianbaobao.algorithm;
+
+/***
+ * @description Main引导类
+ * @author <h>tiantianbaobao</h>
+ * @date 2020-11-13 15:59
+ * @since V1.0.0
+ */
+public class Bootstrap {
+    public static void main(String[] args) {
+        System.out.println("Hi~ This is my homework about algorithm for training camp week 1");
+    }
+}
